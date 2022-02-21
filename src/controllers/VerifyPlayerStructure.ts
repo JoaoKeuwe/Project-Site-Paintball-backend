4d6b9c13818ddb9a70591575bd6903a967f6739a
Fernando Serpa <serpaef@outlook.com>
2022-02-21 18:52:08 -0300

    refactor: Pequenos ajustes de legibilidade do código

--- src/controllers/VerifyPlayerStructure.ts
+++ src/controllers/VerifyPlayerStructure.ts
@@ -2,38 +2,34 @@ import { Request, Response, NextFunction } from 'express';
 
 export class VerifyPlayerStructure {
   handle(req: Request, res: Response, next: NextFunction) {
-    try {
-      const {
-        playerName, picture, age, description, team, role, 
-        priWeapon, secWeapon,  str, mov, int, aim, eqp, 
-        friendly, regional, state, national, international
-      } = req.body;
+    const {
+      playerName, picture, age, description, team, role,
+      priWeapon, secWeapon, str, mov, int, aim, eqp,
+      friendly, regional, state, national, international,
+    } = req.body;
 
-      let isComplete: boolean = 
-      (playerName && typeof playerName === 'string') 
-      && (picture && typeof picture === 'string')
-      && (age && typeof age === 'number')
-      && (description  && typeof description === 'string')
-      && (team && typeof team === 'string')
-      && (role && typeof role === 'string')
-      && (priWeapon && typeof priWeapon === 'string')
-      && (secWeapon && typeof secWeapon === 'string')
-      && (str && typeof str === 'number')
-      && (mov && typeof mov === 'number')
-      && (int && typeof int === 'number')
-      && (aim && typeof aim === 'number')
-      && (eqp && typeof eqp === 'number')
-      && (friendly && typeof friendly === 'number')
-      && (regional && typeof regional === 'number')
-      && (state && typeof state === 'number')
-      && (national && typeof national === 'number')
-      && (international && typeof international === 'number');
+    let isComplete: boolean =
+      playerName && typeof playerName === 'string'
+      && picture && typeof picture === 'string'
+      && age && typeof age === 'number' 
+      && description && typeof description === 'string' 
+      && team && typeof team === 'string' 
+      && role && typeof role === 'string' 
+      && priWeapon && typeof priWeapon === 'string' 
+      && secWeapon && typeof secWeapon === 'string' 
+      && str && typeof str === 'number' 
+      && mov && typeof mov === 'number' 
+      && int && typeof int === 'number' 
+      && aim && typeof aim === 'number' 
+      && eqp && typeof eqp === 'number' 
+      && friendly && typeof friendly === 'number' 
+      && regional && typeof regional === 'number' 
+      && state && typeof state === 'number' 
+      && national && typeof national === 'number' 
+      && international && typeof international === 'number';
 
-      if (!isComplete) return res.status(400).json('Invalid player format');
+    if (!isComplete) return res.status(400).json('Invalid player format');
 
-      next()
-    } catch (err) {
-      return res.status(500).json('deu bosta menó');
-    }
+    next();
   }
 }
