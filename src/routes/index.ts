65464cb6bc884384e47f88e4ce4938f00bccbf28
Fernando Serpa <serpaef@outlook.com>
2022-02-21 18:18:32 -0300

    feat:
    Implementa rotas de listagem de jogadores.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
+import { GetAllPlayersController } from './../controllers/GetAllPlayersController';
 import { CreatePlayerController } from './../controllers/CreatePlayerController';
+import { GetPlayerController } from '../controllers/GetPlayerController';
 import { VerifyWeapon } from './../controllers/VerifyWeapon';
 import { VerifyRole } from './../controllers/VerifyRole';
 import { VerifyPlayerStructure } from './../controllers/VerifyPlayerStructure';
@@ -12,12 +14,19 @@ main
   .get((_req, res) => res.status(200).json('it\'s alive!'));
 
 player
+  .route('/player/:id')
+  .get(new GetPlayerController().handle);
+
+player
   .route('/player')
   .post(
     new VerifyPlayerStructure().handle,
     new VerifyRole().handle,
     new VerifyWeapon().handle,
-    new CreatePlayerController().handle
+    new CreatePlayerController().handle,
+  )
+  .get(
+    new GetAllPlayersController().handle,
   )
 
 export { main, player };
